c8e0cbf818184b5fc171f6647fc4e83eb1c586d6
unknown <pelumioni25@gmail.com>
2020-09-29 18:52:07 -0700

    dashboard

--- src/app/main/leads/leaddetails/leaddetails.component.ts
+++ src/app/main/leads/leaddetails/leaddetails.component.ts
@@ -1,15 +1,30 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery'
 @Component({
   selector: 'app-leaddetails',
   templateUrl: './leaddetails.component.html',
   styleUrls: ['./leaddetails.component.css']
-})
+}) 
 export class LeaddetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private location:Location) { }
 
   ngOnInit() {
+    $('.showinfo').click(function () {
+      $('#information').show(300);
+      $('.showinfo').hide(0);
+      $('.hideinfo').show(0);
+    });
+    $('.hideinfo').click(function () {
+      $('#information').hide(300);
+      $('.showinfo').show(0);
+      $('.hideinfo').hide(0);
+    });
+  }
+
+  backClicked() {
+    this.location.back();
   }
 
 }

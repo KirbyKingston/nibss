05bf111832a45b5add34d911619c052c05faa563
unknown <pelumioni25@gmail.com>
2020-11-02 03:28:46 -0800

    m

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { MsalGuard } from '@azure/msal-angular';
 import { AuthGuard } from 'src/core/guards/authguard/auth.guard';
 
 
